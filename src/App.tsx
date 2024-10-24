226348ff6b37174d237a7be0ea53e72d598317b8
Benson <Benson-Pierre@outlook.com>
2024-10-24 10:05:25 -0300

    uptadte

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Header from './components/Header'
 import Produtos from './containers/Produtos'
 
 import { GlobalStyle } from './styles'
+import { Provider } from 'react-redux'
+
+import { store } from './store'
 
 export type Produto = {
   id: number
@@ -16,19 +19,13 @@ function App() {
   const [carrinho, setCarrinho] = useState<Produto[]>([])
   const [favoritos, setFavoritos] = useState<Produto[]>([])
 
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/ebac_sports')
-      .then((res) => res.json())
-      .then((res) => setProdutos(res))
-  }, [])
-
-  function adicionarAoCarrinho(produto: Produto) {
-    if (carrinho.find((p) => p.id === produto.id)) {
-      alert('Item já adicionado')
-    } else {
-      setCarrinho([...carrinho, produto])
-    }
-  }
+  // function adicionarAoCarrinho(produto: Produto) {
+  //   if (carrinho.find((p) => p.id === produto.id)) {
+  //     alert('Item já adicionado')
+  //   } else {
+  //     setCarrinho([...carrinho, produto])
+  //   }
+  // }
 
   function favoritar(produto: Produto) {
     if (favoritos.find((p) => p.id === produto.id)) {
@@ -40,18 +37,17 @@ function App() {
   }
 
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <div className="container">
-        <Header favoritos={favoritos} itensNoCarrinho={carrinho} />
+        <Header favoritos={favoritos} />
         <Produtos
-          produtos={produtos}
+          // produtos={produtos}
           favoritos={favoritos}
           favoritar={favoritar}
-          adicionarAoCarrinho={adicionarAoCarrinho}
         />
       </div>
-    </>
+    </Provider>
   )
 }
 
--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,21 +1,25 @@
+import { useDispatch } from 'react-redux'
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
 
 import * as S from './styles'
+import { useGetJogosQuery } from '../services/api'
+
 
 type Props = {
-  produtos: ProdutoType[]
   favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
   favoritar: (produto: ProdutoType) => void
 }
 
 const ProdutosComponent = ({
-  produtos,
   favoritos,
-  adicionarAoCarrinho,
   favoritar
 }: Props) => {
+
+  const {data: produtos ,isLoading} = useGetJogosQuery()
+
+  if(isLoading)return <h2>Carregando...</h2>
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
     const IdsDosFavoritos = favoritos.map((f) => f.id)
@@ -26,13 +30,12 @@ const ProdutosComponent = ({
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
             favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
           />
         ))}
       </S.Produtos>
